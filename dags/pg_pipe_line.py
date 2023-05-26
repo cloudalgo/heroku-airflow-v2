2cb79572d4025983af27a948ffe14241931c93ea
xenotime <sandeepkhoj@gmail.com>
2023-05-26 13:52:49 +0530

    using redis for store last run job

--- dags/pg_pipe_line.py
+++ dags/pg_pipe_line.py
@@ -20,6 +20,8 @@ import json
 import logging
 import os
 from psycopg2.extras import execute_values
+from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.exceptions import AirflowException
 
 CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -34,13 +36,14 @@ default_args = {
 # Function to extract data from the source database
 def extract_data(source_conn, source_schema, source_table, created_at_col, modified_at_col, last_run):
     logging.info(f"Extracting data from {source_schema}.{source_table} table...")
+    logging.info(f"Last run data {last_run}")
     hook = PostgresHook(postgres_conn_id=source_conn)
     connection = hook.get_conn()
     cursor = connection.cursor()
 
     if last_run:
         # Filter records created after the last run
-        sql = f"SELECT * FROM {source_schema}.{source_table} WHERE WHERE {created_at_col} >= '{last_run}' OR {modified_at_col} >= '{last_run}';"
+        sql = f"SELECT * FROM {source_schema}.{source_table} WHERE {created_at_col} >= '{last_run}' OR {modified_at_col} >= '{last_run}';"
     else:
         # Extract all records if last run is not provided
         sql = f"SELECT * FROM {source_schema}.{source_table};"
@@ -78,6 +81,8 @@ def transform_data(data, column_mapping):
 def load_data(target_conn, target_schema, target_table, transformed_data, insert_columns, primary_key):
     logging.info(f"Loading data into {target_schema}.{target_table} table...")
     logging.info(insert_columns)
+    logging.info(transformed_data)
+    logging.info(primary_key)
     hook = PostgresHook(postgres_conn_id=target_conn)
     connection = hook.get_conn()
     cursor = connection.cursor()
@@ -89,25 +94,11 @@ def load_data(target_conn, target_schema, target_table, transformed_data, insert
     # Execute the insert statement with transformed data
     # hook.insert_rows(table=f'{target_schema}.{target_table}', rows=transformed_data, insert_statement=insert_statement)
     
-    # Create a temporary table to hold the transformed data
-    temp_table_name = f"temp_{target_table}"
-    create_temp_table_sql = f"""
-        CREATE TEMPORARY TABLE {temp_table_name} (LIKE {target_schema}.{target_table})
-    """
-    cursor.execute(create_temp_table_sql)
-
-    # Populate the temporary table with the transformed data
-    execute_values(cursor, f"INSERT INTO {temp_table_name} ({', '.join(insert_columns)}) VALUES %s", transformed_data)
-
-    # Perform the upsert operation using the temporary table
-    upsert_sql = f"""
-        INSERT INTO {target_schema}.{target_table} ({', '.join(insert_columns)})
-        SELECT {', '.join(insert_columns)}
-        FROM {temp_table_name}
-        ON CONFLICT ({primary_key})
-        DO UPDATE SET ({', '.join(insert_columns[1:])}) = ({', '.join(f'EXCLUDED.{col}' for col in insert_columns[1:])})
-    """
-    cursor.execute(upsert_sql)
+    # Generate the insert SQL statement with specified columns
+    insert_sql = f'INSERT INTO {target_schema}.{target_table} ({", ".join(insert_columns)}) VALUES %s'
+    
+    # Execute the insert statement with transformed data
+    execute_values(cursor, insert_sql, transformed_data)
 
     connection.commit()
     cursor.close()
@@ -119,19 +110,56 @@ def read_table_mappings(file_path):
         table_mappings = json.load(f)
     return table_mappings
 
+# Function to retrieve the last run timestamp from Redis
+def get_last_run(redis_conn, key):
+    last_run = None
+    try:
+        last_run = redis_conn.get(key)
+        logging.info(f"last_run: {last_run}")
+        if last_run:
+            last_run = last_run.decode()  # Convert bytes to string
+    except AirflowException as e:
+        logging.error(f"Error retrieving last run from Redis: {str(e)}")
+    return last_run
+
+# Function to store the last run timestamp in Redis
+def set_last_run(redis_conn, key, value):
+    try:
+        redis_conn.set(key, value)
+    except AirflowException as e:
+        logging.error(f"Error storing last run in Redis: {str(e)}")
+
 # Function to transfer data for a specific table
-def transfer_table_data(source_conn, source_schema, source_table, target_conn, target_schema, target_table, column_mapping, insert_columns, created_at_col, modified_at_col, primary_key, last_run=None):
+def transfer_table_data(source_conn, source_schema, source_table, target_conn, target_schema, target_table, column_mapping, insert_columns, created_at_col, modified_at_col):
     logging.info(f"Transfer process started for {source_schema}.{source_table} table to {target_schema}.{target_table} table.")
     
-    # Step 1: Extract data from the source table
+     # Step 1: Retrieve the last run information from Redis
+    try:
+        redis_hook = RedisHook(redis_conn_id='redis_default')
+        redis_conn = redis_hook.get_conn()
+        last_run = get_last_run(redis_conn, f'last_run:{source_schema}.{source_table}')
+    except AirflowException as e:
+        logging.error(f"Error retrieving last run from Redis: {str(e)}")
+        last_run = None
+
+    # Step 2: Extract data from the source table
     data = extract_data(source_conn, source_schema, source_table, created_at_col, modified_at_col, last_run)
 
-    # Step 2: Transform the extracted data
+    # Step 3: Transform the extracted data
     transformed_data = transform_data(data, column_mapping)
 
-    # Step 3: Load the transformed data into the target table
+    # Step 4: Load the transformed data into the target table
     load_data(target_conn, target_schema, target_table, transformed_data, insert_columns, primary_key)
 
+    # Step 5: Store the last run information in Redis
+    try:
+        set_last_run(redis_conn, f'last_run:{source_schema}.{source_table}', datetime.utcnow().isoformat())
+    except AirflowException as e:
+        logging.error(f"Error storing last run in Redis: {str(e)}")
+
+    if redis_conn:
+        redis_conn.close()
+    
     logging.info(f"Transfer process completed for {source_schema}.{source_table} table to {target_schema}.{target_table} table.")
 
 # Define the path to the table mappings JSON file
@@ -141,7 +169,7 @@ table_mappings_file = f'{CUR_DIR}/mapping/table_mappings.json'
 table_mappings = read_table_mappings(table_mappings_file)
 
 with DAG(
-    dag_id='pg_data_transfer',
+    dag_id='pg_data_transfer_1',
     default_args=default_args,
     schedule_interval=None
 ) as dag:
@@ -175,11 +203,8 @@ with DAG(
                 'column_mapping': column_mapping,
                 'insert_columns': insert_columns,
                 'created_at_col': created_at_col,
-                'modified_at_col': modified_at_col,
-                "primary_key": primary_key,
-                'last_run': '{{ prev_execution_date.isoformat() if prev_execution_date else None }}'
-            },
-            provide_context=True
+                'modified_at_col': modified_at_col
+            }
         )
 
         # Set task dependencies
