4779e8e8723b2786f9c4d749114f089f304b4a0c
xenotime <sandeepkhoj@gmail.com>
2023-06-02 12:38:05 +0530

    - updated example

--- dags/level1/data_pipeline.py
+++ dags/level1/data_pipeline.py
@@ -1,13 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 import json
 import csv
 import boto3
+import os
+
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 
 default_args = {
-    'owner': 'your_name',
+    'owner': 'airflow',
     'start_date': datetime(2023, 5, 30),
     'depends_on_past': False,
     'email_on_failure': False,
@@ -18,9 +22,9 @@ default_args = {
 
 dag = DAG('data_pipeline', default_args=default_args, schedule_interval=None)
 
-def load_data_from_postgres(source_table):
-    pg_hook = PostgresHook(postgres_conn_id='postgres_default')
-    select_sql = f"SELECT * FROM {source_table}"
+def load_data_from_postgres(connection_id, schema, source_table):
+    pg_hook = PostgresHook(postgres_conn_id=connection_id)
+    select_sql = f"SELECT * FROM {schema}.{source_table}"
     connection = pg_hook.get_conn()
     cursor = connection.cursor()
     cursor.execute(select_sql)
@@ -48,37 +52,44 @@ def transform_row_to_json(rows):
         transformed_rows.append(json.dumps(transformed_row))
     return transformed_rows
 
-def insert_rows_to_destination(rows, dest_table):
+def insert_rows_to_destination(connection_id, schema, rows, dest_table):
     insert_sql = f"""
-        INSERT INTO {dest_table} (created_at, modified_at, data)
+        INSERT INTO {schema}.{dest_table} (created_at, modified_at, data)
         VALUES (%s, %s, %s)
     """
-    pg_hook = PostgresHook(postgres_conn_id='postgres_default')
+    pg_hook = PostgresHook(postgres_conn_id=connection_id, schema=schema)
     for row in rows:
         pg_hook.run(insert_sql, parameters=(row['created_at'], row['modified_at'], row['data']))
 
 # Load configuration from a JSON file
-with open('table_sync_config.json') as config_file:
+with open(f"{CUR_DIR}/table_sync_config.json") as config_file:
     config_data = json.load(config_file)
 
 for config in config_data:
     source_type = config['source_type']
+    source_connection = config['source_connection']
+    source_schema = config['source_schema']
+    source_table = config['source_table']
+    dest_connection = config['dest_connection']
+    dest_schema = config['dest_schema']
+    dest_table = config['dest_table']
+
     if source_type == 'postgres':
-        rows = load_data_from_postgres(config['source_table'])
+        rows = load_data_from_postgres(source_connection, source_schema, source_table)
     elif source_type == 'csv':
         rows = load_data_from_csv(config['s3_bucket'], config['s3_key'])
 
     transform_task = PythonOperator(
-        task_id=f'transform_{source_type}_to_json_{config["dest_table"]}',
+        task_id=f'transform_{source_type}_to_json_{dest_table}',
         python_callable=transform_row_to_json,
         op_kwargs={'rows': rows},
         dag=dag
     )
 
     insert_task = PythonOperator(
-        task_id=f'insert_{source_type}_to_{config["dest_table"]}',
+        task_id=f'insert_{source_type}_to_{dest_table}',
         python_callable=insert_rows_to_destination,
-        op_kwargs={'rows': rows, 'dest_table': config['dest_table']},
+        op_kwargs={'connection_id': dest_connection, 'schema': dest_schema, 'rows': rows, 'dest_table': dest_table},
         dag=dag
     )
 
